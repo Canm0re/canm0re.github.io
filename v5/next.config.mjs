17f824ef014cb96ded460482d34070ed64383138
Canmore <140160791+Canm0re@users.noreply.github.com>
2025-01-05 22:27:25 +0000

    added stuff for Pages

--- v5/next.config.mjs
+++ v5/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',  // Enable static exports
+  basePath: '/canm0re.github.io', // Replace with your repository name
+  images: {
+    unoptimized: true, // Required for static export
+  }
+};
 
 export default nextConfig;
